b676423ef5a4ac9a26e24bb404a5a196b25ba1ab
Dimon <yoh60438@gmail.com>
2023-03-18 14:58:06 +0700

    add App.vue

---
+++ App.vue
@@ -0,0 +1,30 @@
+<!--Template-->
+<template>
+    <div id="app">
+        <h1>My Todo App!</h1>
+        <TodoList/>
+    </div>
+</template>
+
+
+<!--Script-->
+<script>
+//import TodoList-component
+import TodoList from 'components/TodoList.vue'
+
+//export TodoList-component
+export default {
+    components: {
+        TodoList
+    }
+}
+</script>
+
+<style lang="scss">
+@import './variables.scss';
+
+*, *::before, *::after {
+    box-sizing: border-box;
+}
+
+</style>
