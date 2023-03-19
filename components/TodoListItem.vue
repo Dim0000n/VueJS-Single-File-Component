5ffee55d6741928a822e368b903faa6b7e73705b
Dimon <yoh60438@gmail.com>
2023-03-19 22:38:25 +0700

    add todolistitem.vue

---
+++ components/TodoListItem.vue
@@ -0,0 +1,21 @@
+<!--template-->
+<template>
+    <li>
+        {{ todo.text }}
+        <button @click="$emit('remove', todo.id)">
+            X
+        </button>
+    </li>
+</template>
+
+<!--script-->
+<script>
+export default {
+    props: {
+        todo: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
